84815a7f435774b27df22a55993c93e11bf7acdf
cordeirojp <jean-pierre.cordeiro@ca-ts.fr>
2019-08-06 10:26:10 +0200

    step 2

--- mySigmaProjetOne/lambda.js
+++ mySigmaProjetOne/lambda.js
@@ -1,4 +1,10 @@
+let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
     
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['message'];
+    console.log("Sending message", message, "to receiver", receiver);
+
     callback(null, {"message": "Successfully executed"});
 }
